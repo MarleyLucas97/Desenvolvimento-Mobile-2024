fb944c16b1960fab9ed597cc0e2117bf106ed692
Marley De Lucas <marleydelucas97@gmail.com>
2024-03-26 09:26:29 -0300

    03.26.24 part2

--- Aula 01/Kotlin/Exercicios 01/src/Main.kt
+++ Aula 01/Kotlin/Exercicios 01/src/Main.kt
@@ -1,6 +1,20 @@
 import java.util.Scanner
 fun main() {
+    println("Digite o nome do aluno: ")
+    var nome = readln()
+    val x = mutableListOf<Double>()
     for(i in 1..3) {
-        println("Digite a$i: ")
+        println("Digite a $i nota: ")
+        val y = readln().toDouble()
+        x.add(y)
+    }
+    var m = (x[0] + x[1] +x[2]) / 3
+            println("a media das notas e: $m")
+    if (m >=7){
+        println("O Aluno $nome foi Aprovado!!!")
+    }else if(m < 7){
+        println("O Aluno $nome foi Reprovado.")
+    }else{
+        println("Notas invalidas!")
     }
 }
